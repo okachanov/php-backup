dc26c6f768e906e5aa80d692c3a1354d2eb1ca2a
al.germis <al.germis@gmail.com>
2013-02-20 11:05:24 +0200

    multiple db

--- runner.php
+++ runner.php
@@ -4,26 +4,32 @@
 	$aMailConfig	= include(dirname(__FILE__).'/config/mail.config.php');
 
 	$sHost			= $aDbconfig['DatabaseHost'];
-	$sDatbase		= $aDbconfig['DatabaseName'];
+	$aDatbases		= $aDbconfig['DatabaseName'];
 	$sUser			= $aDbconfig['DatabaseUser'];
 	$sPassword		= $aDbconfig['DatabasePassword'];
 	
 	$sDestanation	= "/";
 
 	include('Backup.class.php');
-	$oBackupJob	= new BackupJob($sHost, $sDatbase, $sUser, $sPassword);
-	$oBackupJob->SetMailParams($aMailConfig);
-	$oBackupJob->sBackupMethod = "mail";
 	
-	$bResult	= $oBackupJob->PerformBackup();
+	foreach($aDatbases as $sDatbase){
+		$oBackupJob	= new BackupJob($sHost, $sDatbase, $sUser, $sPassword);
+		$oBackupJob->SetMailParams($aMailConfig);
+		$oBackupJob->sBackupMethod = "mail";
 	
-	if($bResult){
-		print "ok";
-	}else{
-		$aErrors = $oBackupJob->GetErrors();
-		print "<pre>";
-		print_r($aErrors);
-		print "<pre>";
+		$bResult	= $oBackupJob->PerformBackup();
+		
+		if($bResult){
+			;
+		}else{
+			$aErrors = $oBackupJob->GetErrors();
+			print "<pre>";
+			print_r($aErrors);
+			print "<pre>";
+		}
 	}
+
+	
+
 
 ?>
